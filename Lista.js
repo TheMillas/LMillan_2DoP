a0a8daee9fe391b54c0b658f302f7a2715a93871
TheMillas <92330030+TheMillas@users.noreply.github.com>
2023-04-19 17:12:10 -0500

    Tarea agregada

---
+++ Lista.js
@@ -0,0 +1,9 @@
+let tareas=["Jugar xbox","realizar tarea de programación","nadar"];
+// INSERTAR CÓDIGO PARA AGREGAR ELEMENTO AL ARREGLO
+tareas.push("Ir al cine","Estudiar para Ing. de SW II");
+
+console.log("Mi lista de actividades");
+console.log("************"); 
+for(let i = 0; i < tareas.length; i++){
+    console.log((i+1)+ "."+"-" + tareas[i]); 
+}
